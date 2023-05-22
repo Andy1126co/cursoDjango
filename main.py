d8d3727e1b2446ac378816a49e7aba4b7fd0d008
Andy1126 <dangars@misena.edu.co>
2023-05-22 18:47:15 -0500

    ejercicios

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+# EJERCICIO 1
+#
+# El area metropolitana de bucaramanga esta constituido
+# por los municipio de Bucaramanga, Floridablanca, Girón,
+# y Piedecueta
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ¿Cuantas personas en
+# total hay en el AMB?
+#
+# Bucaramanga  581 mil
+# Flroridablanca 267 mil
+# Girón            260 mil
+# Piedecuesta       163 mil
+#
+#
+#
+#
+#

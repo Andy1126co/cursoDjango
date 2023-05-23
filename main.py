f2035a60598cb68c371353d8fe8b4d8c13524e95
Daniel <dangars@misena.edu.co>
2023-05-23 14:09:52 -0500

    calculos primeros ejercicios

--- main.py
+++ main.py
@@ -11,8 +11,23 @@
 # Flroridablanca 267 mil
 # Girón            260 mil
 # Piedecuesta       163 mil
-#
-#
-#
-#
-#
+
+
+bucaramanga = 581000
+floridablanca = 267000
+giron = 260000
+piedecuesta = 163000
+
+total = bucaramanga + floridablanca + giron + piedecuesta
+print("En el area metropolitana hay un total de " + str(total) + " Habitantes")
+
+# ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Jose tiene 3 hijos, si cada hijo tiene 2 mascotas,
+# ¿cuántas mascotas tiene Jose en su hogar?
+
+hijosJose = 3
+mascotas = 2
+print("Jose tiene " + str(hijosJose * mascotas) + " mascotas en su hogar")
+print("¿cuántas mascotas habrían en el AMB?")
+print("Hay " + str(int((total / 10) * 3)) + " mascotas en el AMB")
